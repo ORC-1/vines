0c9acad8b67570d189f8cdc8535955a242706f34
ORC-1 <Rolandazim@gmail.com>
2018-06-21 03:50:07 +0100

    Url config

--- vines/settings.py
+++ vines/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*.*']
+ALLOWED_HOSTS = ['vinef.herokuapp.com' ,'127.0.0.1']
 
 
 # Application definition
